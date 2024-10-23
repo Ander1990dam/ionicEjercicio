3dda1fbafcac215a7167af3e591272a32dd5ba9a
Ander <ander1990dam@gmail.com>
2024-10-23 09:05:10 +0200

    update

--- src/app/servicios/shared.service.ts
+++ src/app/servicios/shared.service.ts
@@ -28,16 +28,16 @@ export class SharedService {
         { title: 'Inicio', url: '/inicio', icon: 'home', enabled: true },
         { title: 'Alta', url: '/alta', icon: 'person-add', enabled: true },
         { title: 'Foto', url: '/photo', icon: 'camera', enabled: true },
-        { title: 'Galeria', url: '/galeria', icon: 'image', enabled: true },
+        //{ title: 'Galeria', url: '/galeria', icon: 'image', enabled: true },
       ];
     } else if (rol === 'administrador') {
       pages = [
         { title: 'Inicio', url: '/inicio', icon: 'home', enabled: true },
         { title: 'Alta', url: '/alta', icon: 'person-add', enabled: true },
-        { title: 'Admin', url: '/panel-admin', icon: 'create', enabled: true },
+       // { title: 'Admin', url: '/panel-admin', icon: 'create', enabled: true },
         { title: 'Admin Cursos', url: '/panel-admin-cursos', icon: 'create', enabled: true },
         { title: 'Foto', url: '/photo', icon: 'camera', enabled: true },
-        { title: 'Galeria', url: '/galeria', icon: 'image', enabled: true },
+       // { title: 'Galeria', url: '/galeria', icon: 'image', enabled: true },
       ];
     } else {
       pages = [{ title: 'Inicio', url: '/inicio', icon: 'home', enabled: true }];
